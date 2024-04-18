e6dc5faf98fbd08555b1a04891e03eb2b7b4b532
Jason <s160420070@student.ubaya.ac.id>
2024-04-19 01:48:34 +0700

    UTS, unfinished 2

--- app/src/main/java/com/example/jason_anmp_160420070/view/FragmentNewsDetails.kt
+++ app/src/main/java/com/example/jason_anmp_160420070/view/FragmentNewsDetails.kt
@@ -5,13 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.lifecycle.ViewModelProvider
+import androidx.navigation.Navigation
 import com.example.jason_anmp_160420070.R
+import com.example.jason_anmp_160420070.databinding.FragmentNewsDetailsBinding
+import com.example.jason_anmp_160420070.viewmodel.NewsListViewModel
+import com.example.jason_anmp_160420070.viewmodel.UserDetailViewModel
 
 class FragmentNewsDetails : Fragment() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
     }
 
     override fun onCreateView(
@@ -19,7 +23,18 @@ class FragmentNewsDetails : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_news_details, container, false)
+        val binding = FragmentNewsDetailsBinding.inflate(inflater, container, false)
+        val newsId = FragmentNewsDetailsArgs.fromBundle(requireArguments()).newsId
+
+        var newsDetailsViewModel = ViewModelProvider(this).get(NewsListViewModel::class.java)
+        newsDetailsViewModel.fetch(newsId)
+
+        val newsData = newsDetailsViewModel.newsSingularLD.value
+        binding.textViewTitle.setText(newsData?.title)
+        binding.textViewAuthor.setText("By: " + newsData?.author)
+        binding.textViewNewsText.setText(newsData?.newsText)
+
+        return binding.root
     }
 
 }
--- app/src/main/java/com/example/jason_anmp_160420070/viewmodel/NewsListViewModel.kt
+++ app/src/main/java/com/example/jason_anmp_160420070/viewmodel/NewsListViewModel.kt
@@ -3,18 +3,33 @@ package com.example.jason_anmp_160420070.viewmodel
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import com.example.jason_anmp_160420070.model.News
+import com.example.jason_anmp_160420070.model.User
 
 class NewsListViewModel : ViewModel() {
     val newsLD = MutableLiveData<ArrayList<News>>()
+    val newsSingularLD = MutableLiveData<News>()
     val newsLoadErrorLD = MutableLiveData<Boolean>()
     val loadingLD = MutableLiveData<Boolean>()
 
     fun refresh() {
+        newsLoadErrorLD.value = true
+        loadingLD.value = true
+
         newsLD.value = arrayListOf(
             News(1,"Orange cat racism", "Not an Orange", "Orange cats face persecution..", "test1"),
             News(2,"Black cats are best!", "HOUND", "Black cats, often seen as a sign of bad luck..", "test2"),
             News(3,"The Male Calico", "Cat News Network", "Calicos are nearly all female. However,..", "test3")
         )
+
+        newsLoadErrorLD.value = false
+        loadingLD.value = false
+    }
+
+    fun fetch(newsId: Int){
+        newsLoadErrorLD.value = true
+        loadingLD.value = true
+
+        newsSingularLD.value = News(1,"Orange cat racism", "Not an Orange", "Orange cats face persecution..", "test1")
 
         newsLoadErrorLD.value = false
         loadingLD.value = false
